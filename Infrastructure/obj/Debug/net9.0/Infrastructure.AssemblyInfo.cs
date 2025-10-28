a944350875cc42b6b8b1e009d2f425164a81af83
LaurieBassett-Jones <laurie.bassett-jones@mab.org.uk>
2025-10-28 10:00:36 +0000

    perform migration to make 3 properties nullable

--- Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e27dfd38b8d25e5d46ed73a2382c6e757dd09ce1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c9b66784913a9d7ab813e18fd6331b4c758e928")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
