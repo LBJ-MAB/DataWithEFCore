deff1393b0473313c904dd4bbf1eef92f179d23e
LaurieBassett-Jones <laurie.bassett-jones@mab.org.uk>
2025-10-30 11:27:27 +0000

    using a lambda expression for MapPost method

--- DataWithEFCore.Api/Program.cs
+++ DataWithEFCore.Api/Program.cs
@@ -21,10 +21,7 @@ var tasks = app.MapGroup("/tasks");
 tasks.MapGet("/", async (ITaskService service) => await service.GetAllTasks());
 tasks.MapGet("/{id}", async (ITaskService service, int id) => await service.GetTaskById(id: id));
 tasks.MapGet("/complete", async (ITaskService service) => await service.GetCompleteTasks());
-tasks.MapPost("/", async (ITaskService service, [FromBody] TaskItem task) =>
-{
-    return await service.AddTask(task: task);
-});
+tasks.MapPost("/", async (ITaskService service, [FromBody] TaskItem task) => await service.AddTask(task: task));
 tasks.MapPut("/{id}", async (ITaskService service, int id, [FromBody] TaskItem inputTask) => await service.UpdateTask(id: id, inputTask: inputTask));
 tasks.MapDelete("/{id}", async (ITaskService service, int id) => await service.DeleteTask(id: id));
 
