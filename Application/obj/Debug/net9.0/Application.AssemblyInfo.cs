d67e0a24fe607ee921fe24fe868620ebe5008d5a
LaurieBassett-Jones <laurie.bassett-jones@mab.org.uk>
2025-11-04 14:04:55 +0000

    initial commit

--- Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adae156b2f8f7684296ac277e67b22d47c1db69c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6f1ec957b6129254e1f6c9e5901c964ef3b0cf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
